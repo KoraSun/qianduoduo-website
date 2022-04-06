f32a47d24c714ef3df6977b574da636d151fdce1
kora <bettm9706@gmail.com>
2022-04-06 16:02:41 +0800

    deploy

---
+++ precache-manifest.e7074c3f67ae5d77b861ff6f037782ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931793c78ff3a0ca0ea0",
+    "url": "/qianduoduo-website/css/app.7cf97fb6.css"
+  },
+  {
+    "revision": "0a40f07fea10eca7a0b0c3fd9bf722ce",
+    "url": "/qianduoduo-website/index.html"
+  },
+  {
+    "revision": "931793c78ff3a0ca0ea0",
+    "url": "/qianduoduo-website/js/app.5471183a.js"
+  },
+  {
+    "revision": "9ac4869c77bc829fd8b2",
+    "url": "/qianduoduo-website/js/chunk-vendors.61cb2f65.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/qianduoduo-website/manifest.json"
+  },
+  {
+    "revision": "e38b6b1a9bfcd1dd9df44a1f41f9c5ad",
+    "url": "/qianduoduo-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/qianduoduo-website/robots.txt"
+  }
+]);
